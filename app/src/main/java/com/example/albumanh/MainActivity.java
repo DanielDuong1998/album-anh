a56e15f8a5e28236dbf22b5930def85fdd6f7d68
DanielDuong1998 <dvkhangnt@gmail.com>
2020-12-11 23:46:24 +0700

    load all image and video, sort by time

--- app/src/main/java/com/example/albumanh/MainActivity.java
+++ app/src/main/java/com/example/albumanh/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.albumanh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.albumanh.Static.MediaModel;
 
 public class MainActivity extends AppCompatActivity {
+  private final int REQUEST_CODE_STORAGE = 567;
+  private final int REQUEST_CODE_CAMERA = 123;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.albums);
+    setContentView(R.layout.activity_all_media_item);
+
+    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if(requestCode == REQUEST_CODE_STORAGE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+      MediaModel.init(this);
+
+    } else if(requestCode == REQUEST_CODE_CAMERA && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+    } else{
+      Toast.makeText(MainActivity.this, "Permission denied !", Toast.LENGTH_SHORT).show();
+    }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
   }
 }
---
+++ app/src/main/java/com/example/albumanh/MediaItem.java
@@ -0,0 +1,76 @@
+package com.example.albumanh;
+
+public class MediaItem {
+  private String path;
+  private String name;
+  private String dateAdded;
+  private String album;
+  private long size;
+  private boolean isCheck;
+  private int type; // type = 1 => image, type = 2 => video
+
+  public MediaItem(String path, String name, String dateAdded, String album, long size, int type) {
+    this.path = path;
+    this.name = name;
+    this.dateAdded = dateAdded;
+    this.album = album;
+    this.size = size;
+    this.isCheck = false;
+    this.type = type;
+  }
+
+  //get
+  public String getPath() {
+    return path;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDateAdded() {
+    return dateAdded;
+  }
+
+  public String getAlbum() {
+    return album;
+  }
+
+  public long getSize() {
+    return size;
+  }
+
+  public boolean isCheck() {
+    return isCheck;
+  }
+
+  public int getType() {
+    return type;
+  }
+
+  //set
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDateAdded(String dateAdded) {
+    this.dateAdded = dateAdded;
+  }
+
+  public void setAlbum(String album) {
+    this.album = album;
+  }
+
+  public void setSize(long size) {
+    this.size = size;
+  }
+
+  public void setCheck(boolean check) {
+    isCheck = check;
+  }
+
+}
